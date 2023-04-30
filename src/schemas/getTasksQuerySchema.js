94a2659166ea6406cb70fb951d7e1fe81eb6efec
Marc Maycas <marc.maycas@gmail.com>
2023-04-30 12:06:55 +0200

    Added validation for multiple statuses

--- src/schemas/getTasksQuerySchema.js
+++ src/schemas/getTasksQuerySchema.js
@@ -1,12 +1,17 @@
-const Joi = require("joi").extend(require('@joi/date'))
-const { STATUS } = require('../utils/constants/constants')
+const Joi = require('joi').extend(require('@joi/date'));
+const { STATUS } = require('../utils/constants/constants');
 
-const getTasksQuerySchema = Joi.object()
-    .keys({
-        datemin: Joi.date().format('YYYY/MM/DD HH:mm:ss'),
-        datemax: Joi.date().format('YYYY/MM/DD HH:mm:ss').greater(Joi.ref('datemin')),
-        search: Joi.string(),
-        status: Joi.string().valid(...Object.values(STATUS))
-    })
+const getTasksQuerySchema = Joi.object().keys({
+	datemin: Joi.date().format('YYYY/MM/DD HH:mm:ss'),
+	datemax: Joi.date().format('YYYY/MM/DD HH:mm:ss').greater(Joi.ref('datemin')),
+	search: Joi.string(),
+	status: Joi.string()
+		.trim()
+		.pattern(
+			new RegExp(
+				`^(${Object.values(STATUS).join('|')})(,(${Object.values(STATUS).join('|')}))*$`
+			)
+		)
+});
 
-module.exports = { getTasksQuerySchema }
+module.exports = { getTasksQuerySchema };
